67bd3744aaf8d659aa51a1d8a321a34e59c76fe5
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2015-07-12 12:23:49 +0200

    Added mux.py containing a set switching components. Added test_mux.py

---
+++ myhdl_lib/mux.py
@@ -0,0 +1,79 @@
+from myhdl import *
+
+
+def mux(sel, ls_di, do):
+    """ Multiplexes a list of input signals to an output signal
+            do = sl_di[sel]
+
+            sel - select index
+            ls_di - list of input signals
+            do - output signals
+            
+    """
+    N = len(ls_di)
+    @always_comb
+    def _mux():
+        do.next = 0
+        for i in range(N):
+            if i == sel:
+                do.next = ls_di[i]
+    return _mux
+
+
+def demux(sel, di, ls_do):
+    """ Demultiplexes an input signal to a list of output signals
+            ls_do[sel] =  di
+
+            sel - select index
+            di - input signal
+            ls_do - list of output signals
+    """
+    N = len(ls_do)
+    @always_comb
+    def _demux():
+        for i in range(N):
+            ls_do.next = 0
+            if i == sel:
+                ls_do.next = di
+    return _demux
+
+
+def ls_mux(sel, lsls_di, ls_do):
+    """ Multiplexes a list of input signal structures to an output structure. 
+        A structure is represented by a list of signals: [signal_1, signal_2, ..., signal_n]
+            ls_do[0] = lsls_di[sel][0]
+            ls_do[1] = lsls_di[sel][1]
+            ...
+            ls_do[n] = lsls_di[sel][n]
+
+            sel - select index
+            lsls_di - list of input signal structures: [[sig, sig, ..., sig], [sig, sig, ..., sig], ..., [sig, sig, ..., sig]]
+            ls_do - output signal structure: [sig, sig, ..., sig]
+    """
+    N = len(ls_do)
+    lsls_in = zip(*lsls_di)
+    return [mux(sel, lsls_in[i], ls_do[i]) for i in range(N)]
+
+
+def ls_demux(sel, ls_di, lsls_do):
+    """ Demultiplexes an input signal structure to list of output structures. 
+        A structure is represented by a list of signals: [signal_1, signal_2, ..., signal_n]
+            lsls_do[sel][0] = ls_di[0]
+            lsls_do[sel][1] = ls_di[1]
+            ...
+            lsls_do[sel][n] = ls_di[n]
+
+            sel - select index
+            ls_di - input signal structure: [sig, sig, ..., sig]
+            lsls_do - list of output signal structures: [[sig, sig, ..., sig], [sig, sig, ..., sig], ..., [sig, sig, ..., sig]]
+    """
+    N = len (ls_di)
+    lsls_out = zip(*lsls_do)
+    return [demux(sel, ls_di[i], lsls_out[i])for i in range(N)]
+
+
+
+
+
+if __name__ == '__main__':
+    pass
---
+++ test/test_mux.py
@@ -0,0 +1,134 @@
+from myhdl import *
+from myhdl_lib.mux import mux
+import myhdl_lib.simulation as sim
+
+import random
+
+
+import unittest
+
+
+class TestMux(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.simulators = ["myhdl", "icarus"]
+
+
+    def testMux5(self):
+        """ MUX: 5 inputs"""
+        DMAX = 100
+        NUM_INPUTS = 5
+
+        def mux_wrap(sel, di_0, di_1, di_2, di_3, di_4, do):
+            ''' Needed when mux is co-simulated as top level'''
+            ls_di = [Signal(intbv(0,min=0,max=DMAX)) for _ in range(NUM_INPUTS)]
+            @always_comb
+            def _assign():
+                ls_di[0].next = di_0
+                ls_di[1].next = di_1
+                ls_di[2].next = di_2
+                ls_di[3].next = di_3
+                ls_di[4].next = di_4
+
+            inst = mux(sel=sel, ls_di=ls_di, do=do)
+            return instances()
+
+        sel = Signal(intbv(0,min=0,max=NUM_INPUTS))
+        ls_di = [Signal(intbv(0,min=0,max=DMAX)) for _ in range(NUM_INPUTS)]
+        do = Signal(intbv(0,min=0,max=DMAX))
+
+        argl = {"sel":sel, "di_0":ls_di[0], "di_1":ls_di[1], "di_2":ls_di[2], "di_3":ls_di[3], "di_4":ls_di[4], "do":do}
+
+        def stim():
+            @instance
+            def _inst():
+                sel.next = 0
+                for i in range(NUM_INPUTS):
+                    ls_di[i].next = 0
+                yield delay(10)
+
+                for _ in range(10):
+                    d = [random.randint(0,DMAX-1) for _ in range(NUM_INPUTS)]
+                    for i in range(NUM_INPUTS):
+                        ls_di[i].next = d[i]
+
+                    for s in range(NUM_INPUTS):
+                        sel.next = s
+                        yield delay(10)
+                        assert d[s] == do, "Mux output (sel={}): expected {}, detected {}".format(sel, d[s], do)
+
+                yield delay(10)
+                raise StopSimulation
+            return _inst
+
+        getDut = sim.DUTer()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+
+            dut=getDut(mux_wrap, **argl)
+            stm = stim()
+            Simulation( dut, stm).run()
+            del dut, stm
+
+
+    def testMux2(self):
+        """ MUX: 2 inputs, boolean Select"""
+        DMAX = 100
+        NUM_INPUTS = 2
+
+        def mux_wrap(sel, di_0, di_1, do):
+            ''' Needed when mux is co-simulated as top level'''
+            ls_di = [Signal(intbv(0,min=0,max=DMAX)) for _ in range(NUM_INPUTS)]
+            @always_comb
+            def _assign():
+                ls_di[0].next = di_0
+                ls_di[1].next = di_1
+
+            inst = mux(sel=sel, ls_di=ls_di, do=do)
+            return instances()
+
+        sel = Signal(bool(0))
+        ls_di = [Signal(intbv(0,min=0,max=DMAX)) for _ in range(NUM_INPUTS)]
+        do = Signal(intbv(0,min=0,max=DMAX))
+
+        argl = {"sel":sel, "di_0":ls_di[0], "di_1":ls_di[1], "do":do}
+
+        def stim():
+            @instance
+            def _inst():
+                sel.next = 0
+                for i in range(NUM_INPUTS):
+                    ls_di[i].next = 0
+                yield delay(10)
+
+                for _ in range(10):
+                    d = [random.randint(0,DMAX-1) for _ in range(NUM_INPUTS)]
+                    for i in range(NUM_INPUTS):
+                        ls_di[i].next = d[i]
+
+                    for s in range(NUM_INPUTS):
+                        sel.next = s
+                        yield delay(10)
+                        assert d[s] == do, "Mux output (sel={}): expected {}, detected {}".format(sel, d[s], do)
+
+                yield delay(10)
+                raise StopSimulation
+            return _inst
+
+        getDut = sim.DUTer()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+
+            dut=getDut(mux_wrap, **argl)
+            stm = stim()
+            Simulation( dut, stm).run()
+            del dut, stm
+
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testMux']
+    unittest.main()
