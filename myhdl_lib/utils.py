322a3207d768277a2974e9b333b36d2e183c55ed
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2015-07-16 22:44:34 +0200

    Added byteorder() and test for it

--- myhdl_lib/utils.py
+++ myhdl_lib/utils.py
@@ -9,5 +9,30 @@ def assign(a,b):
     return _assign
 
 
+def byteorder(bv_di, bv_do, REVERSE=True):
+    """ Reverses the byte order of an input bit-vector
+            bv_di - (i) input bit-vector
+            bv_do - (o) output bit-vector
+    """
+    BITS  = len(bv_di)
+
+    assert (BITS % 8)==0, "byteorder: expects len(bv_in)=8*x, but len(bv_in)={}".format(BITS)
+
+    if REVERSE:
+        BYTES = BITS//8
+        y = ConcatSignal(*[bv_di(8*(b+1),8*b) for b in range(BYTES)])
+
+        @always_comb
+        def _reverse():
+            bv_do.next = y
+
+    else:
+        @always_comb
+        def _pass():
+            bv_do.next = bv_di
+
+    return instances()
+
+
 if __name__ == '__main__':
     pass
---
+++ test/test_byteorder.py
@@ -0,0 +1,63 @@
+import unittest
+
+from myhdl import *
+from myhdl_lib.utils import byteorder
+from myhdl_lib.utils import assign
+import myhdl_lib.simulation as sim
+from mylib import byte_order_reverse
+
+import random
+
+
+class TestByteorder(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.simulators = ["myhdl", "icarus"]
+
+    def test56bits(self):
+        ''' BYTEORDER: 7*8 bits'''
+        BYTES = 7
+        BITS = BYTES*8
+
+        bv_di = Signal(intbv(0)[BITS:])
+        bv_do = Signal(intbv(0)[BITS:])
+
+        argl = {"bv_di":bv_di, "bv_do":bv_do, "REVERSE":True}
+
+        def stim():
+            @instance
+            def _inst():
+                bv_di.next = 0
+                yield delay(10)
+                assert 0 == bv_do, "Byteorder (reverse={}): expected {}, detected {}".format(reverse, 0, bv_do)
+
+                for _ in range(10):
+                    bv_di.next = random.randint(0,bv_di.max-1)
+                    yield delay(10)
+                    if reverse:
+                        for b in range(BYTES):
+                            assert bv_di[(b+1)*8:b*8] == bv_do[(BYTES-b)*8:(BYTES-b-1)*8], "Byteorder byte {}: {} {}".format(b, hex(bv_di,BYTES), hex(bv_do,BYTES))
+                    else:
+                        for b in range(BYTES):
+                            assert bv_di[(b+1)*8:b*8] == bv_do[(b+1)*8:b*8], "Byteorder byte {}: {} {}".format(b, hex(bv_di), hex(bv_do))
+
+                yield delay(10)
+                raise StopSimulation
+            return _inst
+
+        getDut = sim.DUTer()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+            for reverse in [False, True]:
+                argl["REVERSE"] = reverse
+                dut=getDut(byteorder, **argl)
+                stm = stim()
+                Simulation( dut, stm).run()
+                del dut, stm
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
