3fc44ed573584dd8f43f0e43f2f08c2a85eab39f
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2015-07-17 12:13:15 +0200

    WIP: pipeline_control_new inital implementation

--- myhdl_lib/pipeline_control.py
+++ myhdl_lib/pipeline_control.py
@@ -1,14 +1,15 @@
 from myhdl import *
+from utils import assign
 
 def pipeline_control(rst, clk, rx_rdy, rx_vld, tx_rdy, tx_vld, stage_enable, stop_rx=None, stop_tx=None):
     """ Pipeline control unit
-            rx_vld, rx_rdy       - handshake at the pipeline input (front of the pipeline)
-            tx_vld, tx_rdy       - handshake at the pipeline output (back of the pipeline)
-            stage_enable         - (output) vector of enable signals, one signal per stage, that controls the data registration in the stages;
+            rx_rdy, rx_vld,      - (o)(i) handshake at the pipeline input (front of the pipeline)
+            tx_rdy, tx_vld,      - (i)(o) handshake at the pipeline output (back of the pipeline)
+            stage_enable         - (o) vector of enable signals, one signal per stage, that controls the data registration in the stages;
                                    The length of this vector determines the number of stages in the pipeline
-            stop_rx              - (input, optional) vector of signals, one signal per stage; when asserted, the corresponding stage stops consuming data;
+            stop_rx              - (i) optional, vector of signals, one signal per stage; when asserted, the corresponding stage stops consuming data;
                                    allows for multicycle execution in a stage (e.g. consume a data, then process it multiple cycles)
-            stop_tx              - (input, optional) vector of signals, one signal per stage; when asserted, the corresponding stage stops producing data;
+            stop_tx              - (i) optional, vector of signals, one signal per stage; when asserted, the corresponding stage stops producing data;
                                     allows for multicycle execution in a stage (consume multiple data to produce single data )
 
             stop_rx and stop_tx  - If you do not need them, then do not connect them
@@ -56,5 +57,78 @@ def pipeline_control(rst, clk, rx_rdy, rx_vld, tx_rdy, tx_vld, stage_enable, sto
 
 
     return instances()
+
+
+
+def pipeline_control_new(rst, clk, rx_rdy, rx_vld, tx_rdy, tx_vld, stage_enable, stop_rx=None, stop_tx=None):
+    """ Pipeline control unit
+            rx_rdy, rx_vld,      - (o)(i) handshake at the pipeline input (front of the pipeline)
+            tx_rdy, tx_vld,      - (i)(o) handshake at the pipeline output (back of the pipeline)
+            stage_enable         - (o) vector of enable signals, one signal per stage, that controls the data registration in the stages;
+                                   The length of this vector determines the number of stages in the pipeline
+            stop_rx              - (i) optional, vector of signals, one signal per stage; when asserted, the corresponding stage stops consuming data;
+                                   allows for multicycle execution in a stage (e.g. consume a data, then process it multiple cycles)
+            stop_tx              - (i) optional, vector of signals, one signal per stage; when asserted, the corresponding stage stops producing data;
+                                    allows for multicycle execution in a stage (consume multiple data to produce single data )
+
+            stop_rx and stop_tx  - If you do not need them, then do not connect them
+    """
+
+    NUM_STAGES = len(stage_enable)
+
+    if (stop_rx == None):
+        stop_rx = Signal(intbv(0)[NUM_STAGES:])
+
+    if (stop_tx == None):
+        stop_tx = Signal(intbv(0)[NUM_STAGES:])
+
+    assert (len(stop_rx)==NUM_STAGES), "pipeline_control: expects len(stop_rx)=len(stage_enable), but len(stop_rx)={} len(stage_enable)={}".format(len(stop_rx),NUM_STAGES)
+    assert (len(stop_tx)==NUM_STAGES), "pipeline_control: expects len(stop_tx)=len(stage_enable), but len(stop_tx)={} len(stage_enable)={}".format(len(stop_tx),NUM_STAGES)
+
+    FIRST_STAGE = 0
+    LAST_STAGE = NUM_STAGES-1
+
+    stg = NUM_STAGES*[None]
+    rdy = (NUM_STAGES+1)*[Signal(bool(0))]
+    vld = (NUM_STAGES+1)*[Signal(bool(0))]
+    BC = NUM_STAGES*[False]
+
+    rdy[0] = rx_rdy
+    vld[0] = rx_vld
+    rdy[-1] = tx_rdy
+    vld[-1] = tx_vld
+    BC[-1] = True
+
+    for i in range(NUM_STAGES):
+        stg[i] = _stage_ctrl(rst=rst, clk=clk, rx_rdy=rdy[i], rx_vld=vld[i], tx_rdy=rdy[i+1], tx_vld=vld[i+1], stage_en=stage_enable[i], stop_rx=stop_rx[i], stop_tx=stop_tx[i])
+
+    return instances()
+
+def _stage_ctrl(rst, clk, rx_rdy, rx_vld, tx_rdy, tx_vld, stage_en, stop_rx=None, stop_tx=None, BC=False):
+    '''  '''
+    state = Signal(bool(0))
+    a = Signal(bool(0))
+    b = Signal(bool(0))
+    bc_link = state if BC else True
+
+    @always_comb
+    def _comb():
+        a.next = tx_rdy or stop_tx or not bc_link
+        b.next = rx_vld or stop_rx
+        rx_rdy.next = a and not stop_rx
+        tx_vld.next = state and not stop_tx
+        stage_en.next = a and b
+
+    @always_seq(clk.posedge, reset=rst)
+    def _state():
+        if a:
+            state.next = b
+
+    return _comb, _state
+
+
+
+
+
 
 
