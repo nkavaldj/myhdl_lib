5fbd9abd93ca152bba653735958ff128009fdcbf
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2015-07-19 20:53:37 +0200

    Added checksum() and test for it

--- myhdl_lib/stream.py
+++ myhdl_lib/stream.py
@@ -1,6 +1,7 @@
 from myhdl import *
 
-from math import log, floor
+from math import log, floor, ceil
+from myhdl_lib.utils import assign
 
 def bytecount(rst, clk, rx_vld, rx_sop, rx_eop, rx_dat, rx_mty, count, MAX_BYTES=1500 ):
     """ Counts bytes in a stream of packetised data
@@ -42,6 +43,117 @@ def bytecount(rst, clk, rx_vld, rx_sop, rx_eop, rx_dat, rx_mty, count, MAX_BYTES
         count.next = count_s
 
     return instances()
+
+
+def checksum(rst, clk, rx_vld, rx_sop, rx_eop, rx_dat, rx_mty, chksum, sum16=None, sumN=None, init_sum=None, MAX_BYTES=1500):
+    """ Calculates checksum over a stream of packetised data
+            rx_vld   - (i) valid data
+            rx_sop   - (i) start of packet
+            rx_eop   - (i) end of packet
+            rx_dat   - (i) data
+            rx_mty   - (i) empty bits when rx_eop
+            init_sum - (i) initial value for the sum (e.g. sum of the some fields from a packet header)
+            sumN     - (o) optional, plane sum
+            sum16    - (o) optional, 16 bit sum (every time the sum overflows, the overflow is added to the sum)
+            chksum   - (o) optional, checksum (~sum16)
+            MAX_BYTES - a limit: maximum number of bytes that may arrive in a single packet
+        Assumes Big-endian data.
+        The results are ready in the first clock cycle after rx_eop
+    """
+
+    DATA_WIDTH = len(rx_dat) 
+
+    assert DATA_WIDTH%16==0, "checksum: expects len(rx_dat)=16*x, but len(tx_dat)={}".format(DATA_WIDTH)
+
+    NUM_BYTES = DATA_WIDTH // 8
+    NUM_WORDS = DATA_WIDTH // 16
+
+    SUM_WIDTH    = 16 + int(ceil(log(MAX_BYTES/2,2))) + 1
+    SUM16_WIDTH  = 16 + int(ceil(log(NUM_WORDS,2))) + 1
+
+
+    if (init_sum != None):
+        assert len(init_sum)<=16, "checksum: expects len(init_sum)<={}, but len(init_sum)={}".format(16, len(init_sum))
+        SUM_WIDTH    += 1
+        SUM16_WIDTH  += 1
+    else:
+        init_sum = 0
+
+    FF_MASK = intbv(0)[DATA_WIDTH:].max-1
+    mdata = Signal(intbv(0)[DATA_WIDTH:])
+
+    @always_comb
+    def _mask_empty():
+        mdata.next = rx_dat
+        if rx_eop:
+            mdata.next = rx_dat & (FF_MASK<<(rx_mty*8))
+
+    # Slice masked data in 16 bit words
+    mdata16 = [Signal(intbv(0)[16:]) for _ in range(NUM_WORDS)]
+    _ass = [assign(mdata16[w], mdata((w+1)*16, w*16)) for w in range(NUM_WORDS)]
+
+    if sumN!=None:
+        assert len(sumN)>=SUM_WIDTH, "checksum: expects len(sumN)>={}, but len(sumN)={}".format(SUM_WIDTH, len(sumN))
+
+        sumN_reg = Signal(intbv(0)[SUM_WIDTH:])
+
+        @always_seq(clk.posedge, reset=rst)
+        def _accuN():
+            ''' Accumulate '''
+            if (rx_vld):
+                s = 0
+
+                if (rx_sop):
+                    s = int(init_sum)
+                else:
+                    s = int(sumN_reg)
+
+                for w in range(NUM_WORDS):
+                    s += mdata16[w]
+
+                sumN_reg.next = s
+
+        @always_comb
+        def _passN():
+            sumN.next = sumN_reg
+
+
+    if chksum!=None or sum16!=None:
+
+        sum16_reg = Signal(intbv(0)[SUM16_WIDTH:])
+
+        @always_seq(clk.posedge, reset=rst)
+        def _accu16():
+            ''' Accumulate 16 bit words'''
+            if (rx_vld):
+                s = 0
+
+                if (rx_sop):
+                    s = int(init_sum)
+                else:
+                    s = int(sum16_reg)
+
+                for w in range(NUM_WORDS):
+                    s += mdata16[w]
+
+                ss = intbv(s)[SUM16_WIDTH:]
+
+                sum16_reg.next = ss[:2*8] + ss[2*8:]
+
+        if sum16!=None:
+            assert len(sum16)>=16, "checksum: expects len(sum16)>={}, but len(sum16)={}".format(16, len(sum16))
+            @always_comb
+            def _pass16():
+                sum16.next = sum16_reg[16:]
+
+        if chksum!=None:
+            assert len(chksum)>=16, "checksum: expects len(chksum)>={}, but len(chksum)={}".format(16, len(chksum))
+            @always_comb
+            def _invert():
+                chksum.next = ~sum16_reg[16:] & 0xFFFF
+
+    return instances()
+
 
 
 if __name__ == '__main__':
---
+++ test/test_checksum.py
@@ -0,0 +1,232 @@
+import unittest
+
+from myhdl import *
+from myhdl_lib.stream import checksum
+import myhdl_lib.simulation as sim
+
+import random
+from math import log, ceil
+
+
+class TestChecksum(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.simulators = ["myhdl", "icarus"]
+
+
+    def testRand(self):
+        ''' CHECKSUM: Random '''
+        MAX_NUM_BYTES = 200
+        BYTES_PER_WORD = [2,4,6,8]
+        getDut = sim.DUTer()
+
+        def testbench(BYTES_PER_WORD):
+            W16_PER_WORD = BYTES_PER_WORD//2
+            SUM_WIDTH = 16 + int(ceil(log(MAX_NUM_BYTES/2,2))) + 1
+
+            rst = ResetSignal(val=0, active=1, async=False)
+            clk = Signal(bool(0))
+
+            rx_vld = Signal(bool(0))
+            rx_sop = Signal(bool(0))
+            rx_eop = Signal(bool(0))
+            rx_dat = Signal(intbv(0)[BYTES_PER_WORD*8:])
+            rx_mty = Signal(intbv(0, min=0, max=BYTES_PER_WORD))
+            sumN = Signal(intbv(0)[SUM_WIDTH:])
+            sum16 = Signal(intbv(0)[16:])
+            sum16n = Signal(intbv(0)[16:])
+
+            argl = {"rst":rst,
+                    "clk":clk,
+                    "rx_vld":rx_vld,
+                    "rx_sop":rx_sop,
+                    "rx_eop":rx_eop,
+                    "rx_dat":rx_dat,
+                    "rx_mty":rx_mty,
+                    "chksum":sum16n,
+                    "sum16":sum16,
+                    "sumN":sumN,
+                    "MAX_BYTES":MAX_NUM_BYTES}
+
+            dut = getDut(checksum, **argl)
+            clkgen = sim.clock_generator(clk, PERIOD=10)
+            rstgen = sim.reset_generator(rst, clk, RST_LENGTH_CC=3)
+
+            @instance
+            def _stim():
+                rx_vld.next = 0
+                rx_sop.next = 0
+                rx_eop.next = 0
+                rx_dat.next = 0
+                rx_mty.next = 0
+                yield rst.negedge
+                yield clk.posedge
+                S = intbv(0)[SUM_WIDTH:]
+                S16 = S[16:] + S[:16]
+                S16n = ~S16 & 0xFFFF
+                assert S16n==sum16n, "checksum sum16n: expected {}, detected {}".format(hex(S16n), hex(sum16n))
+                assert S16==sum16, "checksum sum16: expected {}, detected {}".format(hex(S16), hex(sum16))
+                assert S==sumN, "checksum sumN: expected {}, detected {}".format(hex(S), hex(sumN))
+
+                for _ in range(10):
+                    bytes = random.randint(1,MAX_NUM_BYTES)
+                    words = int(ceil(float(bytes)/BYTES_PER_WORD))
+                    empty = (BYTES_PER_WORD - bytes%BYTES_PER_WORD)%BYTES_PER_WORD
+                    S = intbv(0)[SUM_WIDTH:]
+                    S16 = S[16:] + S[:16]
+                    S16n = ~S16 & 0xFFFF
+                    for i in range(words):
+                        rx_vld.next = 1
+                        rx_sop.next = (i==0)
+                        rx_eop.next = (i==words-1)
+                        rx_dat.next = random.randint(0,(2**(BYTES_PER_WORD*8))-1)
+                        rx_mty.next = empty if i==words-1 else 0
+                        d = rx_dat.next
+                        if rx_eop.next:
+                            d[:] = d & ((intbv(0)[BYTES_PER_WORD*8:].max-1)<<(8*rx_mty.next))
+                        # Sum the word
+                        for i in range(W16_PER_WORD):
+                            S[:] = S[:] + d[16*(i+1):16*i]
+                        # Resuce sum to 16 bit
+                        S16 = S[:]
+                        while S16>S16[16:]:
+                            S16[:] = S16[16:] + S16[:16]
+                        S16[:] = S16[16:]
+                        # Invert
+                        S16n = ~S16 & 0xFFFF
+
+                        yield clk.posedge
+                        yield delay(1)
+                        assert S16n==sum16n, "checksum sum16n: expected {}, detected {}".format(hex(S16n), hex(sum16n))
+                        assert S16==sum16, "checksum sum16: expected {}, detected {}".format(hex(S16), hex(sum16))
+                        assert S==sumN, "checksum sumN: expected {}, detected {}".format(hex(S), hex(sumN))
+                        # Gap
+                        rx_vld.next = 0
+                        for _ in range(random.randint(0,1)):
+                            yield clk.posedge
+
+                yield clk.posedge
+                raise StopSimulation
+            return instances()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+            for BPW in BYTES_PER_WORD:
+                tb = testbench(BPW)
+                Simulation(tb).run()
+                del tb
+
+
+    def testOverflow(self):
+        ''' CHECKSUM: Overflow '''
+        MAX_NUM_BYTES = 201
+        BYTES_PER_WORD = [2,4,6,8]
+        getDut = sim.DUTer()
+
+        def testbench(BYTES_PER_WORD):
+            W16_PER_WORD = BYTES_PER_WORD//2
+            SUM_WIDTH = 16 + int(ceil(log(MAX_NUM_BYTES/2,2))) + 1
+            INIT_WIDTH = 16
+            SUM_WIDTH += 1
+
+            rst = ResetSignal(val=0, active=1, async=False)
+            clk = Signal(bool(0))
+
+            rx_vld = Signal(bool(0))
+            rx_sop = Signal(bool(0))
+            rx_eop = Signal(bool(0))
+            rx_dat = Signal(intbv(0)[BYTES_PER_WORD*8:])
+            rx_mty = Signal(intbv(0, min=0, max=BYTES_PER_WORD))
+            init_sum = Signal(intbv(0)[INIT_WIDTH:])
+            sumN = Signal(intbv(0)[SUM_WIDTH:])
+            sum16 = Signal(intbv(0)[16:])
+            sum16n = Signal(intbv(0)[16:])
+
+            argl = {"rst":rst,
+                    "clk":clk,
+                    "rx_vld":rx_vld,
+                    "rx_sop":rx_sop,
+                    "rx_eop":rx_eop,
+                    "rx_dat":rx_dat,
+                    "rx_mty":rx_mty,
+                    "init_sum":init_sum,
+                    "chksum":sum16n,
+                    "sum16":sum16,
+                    "sumN":sumN,
+                    "MAX_BYTES":MAX_NUM_BYTES}
+
+            dut = getDut(checksum, **argl)
+            clkgen = sim.clock_generator(clk, PERIOD=10)
+            rstgen = sim.reset_generator(rst, clk, RST_LENGTH_CC=3)
+
+            @instance
+            def _stim():
+                rx_vld.next = 0
+                rx_sop.next = 0
+                rx_eop.next = 0
+                rx_dat.next = 0
+                rx_mty.next = 0
+                yield rst.negedge
+                yield clk.posedge
+                S = intbv(0)[SUM_WIDTH:]
+                S16 = S[16:] + S[:16]
+                S16n = ~S16 & 0xFFFF
+                assert S16n==sum16n, "checksum sum16n: expected {}, detected {}".format(hex(S16n), hex(sum16n))
+                assert S16==sum16, "checksum sum16: expected {}, detected {}".format(hex(S16), hex(sum16))
+                assert S==sumN, "checksum sumN: expected {}, detected {}".format(hex(S), hex(sumN))
+
+                for k in range(10):
+                    bytes = MAX_NUM_BYTES
+                    words = int(ceil(float(bytes)/BYTES_PER_WORD))
+                    empty = (BYTES_PER_WORD - bytes%BYTES_PER_WORD)%BYTES_PER_WORD
+                    init_sum.next = init_sum.max-1-k
+                    S = intbv(init_sum.next)[SUM_WIDTH:]
+                    S16 = S[16:] + S[:16]
+                    S16n = ~S16 & 0xFFFF
+                    for i in range(words):
+                        rx_vld.next = 1
+                        rx_sop.next = (i==0)
+                        rx_eop.next = (i==words-1)
+                        rx_dat.next = rx_dat.max-1-k
+                        rx_mty.next = empty if i==words-1 else 0
+                        d = rx_dat.next
+                        if rx_eop.next:
+                            d = rx_dat.next & ((intbv(0)[BYTES_PER_WORD*8:].max-1)<<(8*rx_mty.next))
+                        # Sum the word
+                        for i in range(W16_PER_WORD):
+                            S[:] = S[:] + d[16*(i+1):16*i]
+                        # Resuce sum to 16 bit
+                        S16 = S[:]
+                        while S16>S16[16:]:
+                            S16[:] = S16[16:] + S16[:16]
+                        S16[:] = S16[16:]
+                        # Invert
+                        S16n = ~S16 & 0xFFFF
+
+                        yield clk.posedge
+                        yield delay(1)
+                        assert S16n==sum16n, "checksum sum16n: expected {}, detected {}".format(hex(S16n), hex(sum16n))
+                        assert S16==sum16, "checksum sum16: expected {}, detected {}".format(hex(S16), hex(sum16))
+                        assert S==sumN, "checksum sumN: expected {}, detected {}".format(hex(S), hex(sumN))
+
+                        init_sum.next = 0
+                        # Gap
+                        rx_vld.next = 0
+                        for _ in range(random.randint(0,1)):
+                            yield clk.posedge
+
+                yield clk.posedge
+                raise StopSimulation
+            return instances()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+            for BPW in BYTES_PER_WORD:
+                tb = testbench(BPW)
+                Simulation(tb).run()
+                del tb
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
