619243390dd933a1fa96500a1037ec2e619cc487
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2015-09-23 21:35:09 +0200

    added __all__ to __init__.py

--- myhdl_lib/__init__.py
+++ myhdl_lib/__init__.py
@@ -6,3 +6,15 @@ from myhdl_lib.handshake import hs_join, hs_fork, hs_mux, hs_demux, hs_arbmux, h
 from myhdl_lib.arbiter import arbiter, arbiter_priority, arbiter_roundrobin
 from myhdl_lib.pipeline_control import pipeline_control
 from myhdl_lib.utils import assign, byteorder
+
+__all__ = [
+           rom, ram_sp_rf, ram_sp_wf, ram_sp_ar,ram_sdp_rf, ram_sdp_wf, ram_sdp_ar, ram_dp_rf, ram_dp_wf, ram_dp_ar,
+           fifo,
+           fifo_speculative,
+           mux, demux, ls_mux, ls_demux, bitslice_select, byteslice_select,
+           hs_join, hs_fork, hs_mux, hs_demux, hs_arbmux, hs_arbdemux,
+           arbiter, arbiter_priority, arbiter_roundrobin,
+           pipeline_control,
+           assign, byteorder
+           ]
+
