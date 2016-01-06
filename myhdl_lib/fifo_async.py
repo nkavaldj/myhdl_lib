f275500721a69a8739c49f3b7e624db0caacd8c0
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2016-01-06 18:16:00 +0100

    Initial commit of fifo_async and its test

---
+++ myhdl_lib/fifo_async.py
@@ -0,0 +1,179 @@
+from myhdl import *
+from mem   import ram_sdp_ar
+
+# def fifo_asynchronous(rst, clk, full, we, din, empty, re, dout, afull=None, aempty=None, afull_th=None, aempty_th=None, ovf=None, udf=None, count=None, count_max=None, depth=None, width=None):
+def fifo_async(wrst, rrst, wclk, rclk, wfull, we, wdata, rempty, re, rdata, depth=None, width=None):
+    ''' Asynchronous FIFO
+
+        Implements the design described in:
+            Clifford E. Cummings, "Simulation and Synthesis Techniques for Asynchronous FIFO Design," SNUG 2002 (Synopsys
+            Users Group Conference, San Jose, CA, 2002) User Papers, March 2002, Section TB2, 2nd paper. Also available at
+            www.sunburst-design.com/papers
+
+        Write side interface:
+            wrst   - reset 
+            wclk   - clock
+            wfull  - full flag, immediate set on 'write', delayed clear on 'read' due to clock domain synchronization
+            we     - write enable
+            wdata  - write data
+        Read side interface
+            rrst   - reset
+            cclk   - clock
+            rempty - empty flag, immediate set on 'read', delayed clear on 'write' due to clock domain synchronization
+            re     - read enable
+            rdata  - read data
+        Parameters
+            depth - fifo depth. If not set, default 4 is used. Must be >=4. Must be power fo 2
+            width - data width. If not set, data with equals len(wdata). Can be [0,1,2,3...)
+                    It is possible to instantiate a fifo with data width 0 (no data) if width=0 or width=None and wdata=None
+    '''
+
+    if (width == None):
+        width = 0
+        if wdata is not None:
+            width = len(wdata)
+
+    if (depth == None):
+        depth = 4
+    assert depth >= 4, "Fifo_async parameter 'depth' must be >= 4 , detected depth={}".format(depth)
+    assert (depth & (depth-1)) == 0, "Fifo_async parameter 'depth' must be 2**n, detected depth={}".format(depth)
+
+
+    full_flg        = Signal(bool(1))
+    empty_flg       = Signal(bool(1))
+    full_val        = Signal(bool(1))
+    empty_val       = Signal(bool(1))
+    we_safe         = Signal(bool(0))
+    re_safe         = Signal(bool(0))
+    rd_ptr          = Signal(intbv(0, min=0, max=depth))
+    wr_ptr          = Signal(intbv(0, min=0, max=depth))
+
+    WIDTH = len(rd_ptr)
+
+    rd_ptr_bin          = Signal(intbv(0)[WIDTH+1:])
+    rd_ptr_bin_new      = Signal(intbv(0)[WIDTH+1:])
+    rd_ptr_gray         = Signal(intbv(0)[WIDTH+1:])
+    rd_ptr_gray_new     = Signal(intbv(0)[WIDTH+1:])
+    rd_ptr_gray_sync1   = Signal(intbv(0)[WIDTH+1:])
+    rd_ptr_gray_sync2   = Signal(intbv(0)[WIDTH+1:])
+    wr_ptr_bin          = Signal(intbv(0)[WIDTH+1:])
+    wr_ptr_bin_new      = Signal(intbv(0)[WIDTH+1:])
+    wr_ptr_gray         = Signal(intbv(0)[WIDTH+1:])
+    wr_ptr_gray_new     = Signal(intbv(0)[WIDTH+1:])
+    wr_ptr_gray_sync1   = Signal(intbv(0)[WIDTH+1:])
+    wr_ptr_gray_sync2   = Signal(intbv(0)[WIDTH+1:])
+
+    @always_comb
+    def safe_read_write():
+        wfull.next      = full_flg
+        rempty.next     = empty_flg
+        we_safe.next    = we and not full_flg
+        re_safe.next    = re and not empty_flg
+
+    @always(wclk.posedge)
+    def sync_r2w():
+        ''' Read-domain to write-domain synchronizer '''
+        if (wrst):
+            rd_ptr_gray_sync1.next = 0
+            rd_ptr_gray_sync2.next = 0
+        else:
+            rd_ptr_gray_sync1.next = rd_ptr_gray
+            rd_ptr_gray_sync2.next = rd_ptr_gray_sync1
+
+    @always(rclk.posedge)
+    def sync_w2r():
+        ''' Write-domain to read-domain synchronizer '''
+        if (rrst):
+            wr_ptr_gray_sync1.next = 0
+            wr_ptr_gray_sync2.next = 0
+        else:
+            wr_ptr_gray_sync1.next = wr_ptr_gray
+            wr_ptr_gray_sync2.next = wr_ptr_gray_sync1
+
+    @always_comb
+    def bin_comb():
+        wr_ptr_bin_new.next = wr_ptr_bin
+        rd_ptr_bin_new.next = rd_ptr_bin
+        if (we_safe):
+            wr_ptr_bin_new.next = (wr_ptr_bin + 1) % wr_ptr_bin.max
+        if (re_safe):
+            rd_ptr_bin_new.next = (rd_ptr_bin + 1) % rd_ptr_bin.max
+
+    @always_comb
+    def gray_comb():
+        wr_ptr_gray_new.next    = (wr_ptr_bin_new >> 1) ^ wr_ptr_bin_new
+        rd_ptr_gray_new.next    = (rd_ptr_bin_new >> 1) ^ rd_ptr_bin_new
+
+    @always_comb
+    def full_empty_comb():
+        empty_val.next  = (rd_ptr_gray_new == wr_ptr_gray_sync2)
+        full_val.next   = (wr_ptr_gray_new[WIDTH] != rd_ptr_gray_sync2[WIDTH]) and \
+                          (wr_ptr_gray_new[WIDTH-1] != rd_ptr_gray_sync2[WIDTH-1]) and \
+                          (wr_ptr_gray_new[WIDTH-1:] == rd_ptr_gray_sync2[WIDTH-1:])
+
+
+    @always(wclk.posedge)
+    def wptr_proc():
+        if (wrst):
+            wr_ptr_bin.next     = 0
+            wr_ptr_gray.next    = 0
+            full_flg.next       = 0
+        else:
+            wr_ptr_bin.next     = wr_ptr_bin_new
+            wr_ptr_gray.next    = wr_ptr_gray_new
+            full_flg.next       = full_val
+
+    @always(rclk.posedge)
+    def rptr_proc():
+        if (rrst):
+            rd_ptr_bin.next     = 0
+            rd_ptr_gray.next    = 0
+            empty_flg.next      = 1
+        else:
+            rd_ptr_bin.next     = rd_ptr_bin_new
+            rd_ptr_gray.next    = rd_ptr_gray_new
+            empty_flg.next      = empty_val
+
+
+    if width>0:
+        #===========================================================================
+        # Memory instance
+        #===========================================================================
+        mem_we      = Signal(bool(0))
+        mem_addrw   = Signal(intbv(0, min=0, max=depth))
+        mem_addrr   = Signal(intbv(0, min=0, max=depth))
+        mem_di      = Signal(intbv(0)[width:0])
+        mem_do      = Signal(intbv(0)[width:0])
+
+
+        # RAM: Simple-Dual-Port, Asynchronous read
+        mem = ram_sdp_ar(   clk     = wclk,
+                            we      = mem_we,
+                            addrw   = mem_addrw,
+                            addrr   = mem_addrr,
+                            di      = mem_di,
+                            do      = mem_do )
+
+        @always_comb
+        def mem_connect():
+            mem_we.next         = we_safe
+            mem_addrw.next      = wr_ptr_bin[WIDTH:]
+            mem_addrr.next      = rd_ptr_bin[WIDTH:]
+            mem_di.next         = wdata
+            rdata.next          = mem_do
+
+
+    return instances()
+
+
+def convert(depth=256, width=8):
+    wrst, rrst, wclk, rclk   = [Signal(bool(0)) for _ in range(4)]
+    wdata, rdata  = [Signal(intbv(0)[width:]) for _ in range(2)]
+    re, we, rempty, wfull = [Signal(bool(0)) for _ in range(4)]
+
+    toVerilog(fifo_async, wrst, rrst, wclk, rclk, wfull, we, wdata, rempty, re, rdata, depth=depth, width=width)
+
+
+
+if __name__ == '__main__':
+    convert()
---
+++ test/test_fifo_async.py
@@ -0,0 +1,342 @@
+import unittest
+import itertools
+
+from myhdl import *
+from myhdl_lib.fifo_async import fifo_async
+import myhdl_lib.simulation as sim
+
+
+class TestFifoAsync(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.simulators = ["myhdl", "icarus"]
+
+    def setUp(self):
+        DATA_RANGE_MIN = 0
+        DATA_RANGE_MAX = 128
+        DEPTH_MAX = 101
+
+        self.wfull = Signal(bool(0))
+        self.we = Signal(bool(0))
+        self.wdata = Signal(intbv(0, min=DATA_RANGE_MIN, max=DATA_RANGE_MAX))
+        self.rempty = Signal(bool(0))
+        self.re = Signal(bool(0))
+        self.rdata = Signal(intbv(0, min=DATA_RANGE_MIN, max=DATA_RANGE_MAX))
+
+        self.wclk = sim.Clock(val=0, period=7, units="ns")
+        self.rclk = sim.Clock(val=0, period=11, units="ns")
+        self.wrst = sim.ResetSync(clk=self.wclk, val=0, active=1)
+        self.rrst = sim.ResetSync(clk=self.rclk, val=0, active=1)
+        self.wclkgen = self.wclk.gen()
+        self.rclkgen = self.rclk.gen()
+
+    def tearDown(self):
+        pass
+
+    def wreset(self):
+        yield self.wrst.pulse(5)
+
+    def rreset(self):
+        yield self.rrst.pulse(5)
+
+    def fifo_write(self, val):
+        yield self.wclk.posedge
+        self.we.next = 1
+        self.wdata.next = val
+        yield self.wclk.posedge
+        self.we.next = 0
+        self.wdata.next = 0
+
+    def fifo_read(self):
+        yield self.rclk.posedge
+        self.re.next = 1
+        yield self.rclk.posedge
+        self.re.next = 0
+
+    def fifo_state_check(self, WFull, REmpty, RData=None):
+        yield delay(1)
+        if  WFull is not None:
+            assert self.wfull==WFull, "Full: expected={}, detected={}".format(WFull, self.wfull)
+        if REmpty is not None:
+            assert self.rempty==REmpty,"Empty: expected={}, detected={}".format(REmpty, self.rempty)
+        if RData!=None:
+            assert self.rdata==RData,"Dout: expected={}, detected={}".format(RData, self.rdata)
+
+
+    def testEmptyFull(self):
+        ''' FIFO: Empty and Full, up and down'''
+        DEPTH = [4, 8, 16]
+
+        def stim(DEPTH):
+            @instance
+            def _inst():
+                data_in = itertools.cycle(xrange(128))
+                data_out = itertools.cycle(xrange(128))
+
+                yield self.rreset()
+                yield self.wreset()
+                yield self.fifo_state_check(WFull=0, REmpty=1)
+
+                for _ in range(5):
+                    # Fill up to DEPTH-1
+                    do = data_out.next()
+                    for i in range(DEPTH-1):
+                        yield self.fifo_write(data_in.next())
+                        yield self.fifo_state_check(WFull=0, REmpty=None, RData=do)
+                    # Fill up to DEPTH
+                    yield self.fifo_write(data_in.next())
+                    yield self.fifo_state_check(WFull=1, REmpty=None, RData=do)
+                    # Drain down to 1
+                    for i in range(DEPTH-1):
+                        yield self.fifo_read()
+                        yield self.fifo_state_check(WFull=None, REmpty=0, RData=data_out.next())
+                    # Drain to 0
+                    yield self.fifo_read()
+                    yield self.fifo_state_check(WFull=0, REmpty=1)
+                    # Write, Read (to move the pointers one step further)
+                    yield self.fifo_write(data_in.next())
+                    for _ in range(3): yield self.rclk.posedge # Waith for clock domain crossing sync and update of empty
+                    yield self.fifo_read()
+                    data_out.next()
+
+                yield self.wclk.posedge
+                yield self.rclk.posedge
+
+                raise StopSimulation
+            return _inst
+
+        getDut = sim.DUTer()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+            for dpt in DEPTH:
+                dut = getDut( fifo_async,
+                              wrst=self.wrst,
+                              rrst=self.rrst,
+                              wclk=self.wclk,
+                              rclk=self.rclk,
+                              wfull=self.wfull,
+                              we=self.we,
+                              wdata=self.wdata,
+                              rempty=self.rempty,
+                              re=self.re,
+                              rdata=self.rdata,
+                              depth=dpt,
+                              width=None
+                            )
+                stm = stim(dpt)
+                Simulation(self.wclkgen, self.rclkgen, dut, stm).run()
+                del dut, stm
+
+
+    def testOverflowUnderflow(self):
+        ''' FIFO: Does not write when full, does not read when empty'''
+        DEPTH = [4, 8, 16]
+
+        def stim(DEPTH):
+            @instance
+            def _inst():
+                data_in = itertools.cycle(xrange(128))
+                data_out = itertools.cycle(xrange(128))
+
+                yield self.rreset()
+                yield self.wreset()
+                yield self.fifo_state_check(WFull=0, REmpty=1)
+
+                for _ in range(5):
+                    # Fill up to DEPTH-1
+                    do = data_out.next()
+                    for i in range(DEPTH-1):
+                        yield self.fifo_write(data_in.next())
+                        yield self.fifo_state_check(WFull=0, REmpty=None, RData=do)
+                    # Fill up to DEPTH
+                    yield self.fifo_write(data_in.next())
+                    yield self.fifo_state_check(WFull=1, REmpty=None, RData=do)
+                    # Write when full
+                    yield self.fifo_write(data_in.next())
+                    yield self.fifo_state_check(WFull=1, REmpty=None, RData=do)
+                    # Drain down to 1
+                    for i in range(DEPTH-1):
+                        yield self.fifo_read()
+                        yield self.fifo_state_check(WFull=None, REmpty=0, RData=data_out.next())
+                    # Drain to 0
+                    yield self.fifo_read()
+                    yield self.fifo_state_check(WFull=0, REmpty=1)
+                    # Read when empty
+                    yield self.fifo_read()
+                    yield self.fifo_state_check(WFull=0, REmpty=1)
+                    data_out.next() # Remove the data written when full
+                    # Write, Read (to move the pointers one step further)
+                    yield self.fifo_write(data_in.next())
+                    for _ in range(3): yield self.rclk.posedge # Waith for clock domain crossing sync and update of empty
+                    yield self.fifo_read()
+                    data_out.next()
+
+                yield self.wclk.posedge
+                yield self.rclk.posedge
+
+                raise StopSimulation
+            return _inst
+
+        getDut = sim.DUTer()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+            for dpt in DEPTH:
+                dut = getDut( fifo_async,
+                              wrst=self.wrst,
+                              rrst=self.rrst,
+                              wclk=self.wclk,
+                              rclk=self.rclk,
+                              wfull=self.wfull,
+                              we=self.we,
+                              wdata=self.wdata,
+                              rempty=self.rempty,
+                              re=self.re,
+                              rdata=self.rdata,
+                              depth=dpt,
+                              width=None
+                            )
+                stm = stim(dpt)
+                Simulation(self.wclkgen, self.rclkgen, dut, stm).run()
+                del dut, stm
+
+
+    def testDataWidth1(self):
+        ''' FIFO: Data width 1'''
+        DEPTH = [4, 8, 16]
+        self.wdata = Signal(bool(0)) # <-- Bool
+        self.rdata = Signal(bool(0)) # <-- Bool
+
+        def stim(DEPTH):
+            @instance
+            def _inst():
+                data_in = itertools.cycle(xrange(2))
+                data_out = itertools.cycle(xrange(2))
+
+                yield self.rreset()
+                yield self.wreset()
+                yield self.fifo_state_check(WFull=0, REmpty=1)
+
+                for _ in range(5):
+                    # Fill up to DEPTH-1
+                    do = data_out.next()
+                    for i in range(DEPTH-1):
+                        yield self.fifo_write(data_in.next())
+                        yield self.fifo_state_check(WFull=0, REmpty=None, RData=do)
+                    # Fill up to DEPTH
+                    yield self.fifo_write(data_in.next())
+                    yield self.fifo_state_check(WFull=1, REmpty=None, RData=do)
+                    # Drain down to 1
+                    for i in range(DEPTH-1):
+                        yield self.fifo_read()
+                        yield self.fifo_state_check(WFull=None, REmpty=0, RData=data_out.next())
+                    # Drain to 0
+                    yield self.fifo_read()
+                    yield self.fifo_state_check(WFull=0, REmpty=1)
+                    # Write, Read (to move the pointers one step further)
+                    yield self.fifo_write(data_in.next())
+                    for _ in range(3): yield self.rclk.posedge # Waith for clock domain crossing sync and update of empty
+                    yield self.fifo_read()
+                    data_out.next()
+
+                yield self.wclk.posedge
+                yield self.rclk.posedge
+
+                raise StopSimulation
+            return _inst
+
+        getDut = sim.DUTer()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+            for dpt in DEPTH:
+                dut = getDut( fifo_async,
+                              wrst=self.wrst,
+                              rrst=self.rrst,
+                              wclk=self.wclk,
+                              rclk=self.rclk,
+                              wfull=self.wfull,
+                              we=self.we,
+                              wdata=self.wdata,
+                              rempty=self.rempty,
+                              re=self.re,
+                              rdata=self.rdata,
+                              depth=dpt,
+                              width=None
+                            )
+                stm = stim(dpt)
+                Simulation(self.wclkgen, self.rclkgen, dut, stm).run()
+                del dut, stm
+
+    def testDatawidth0(self):
+        ''' FIFO: Data width 0 '''
+        DEPTH = [4, 8, 16]
+
+        def stim(DEPTH):
+            @instance
+            def _inst():
+                data_in = itertools.cycle(xrange(128))
+                data_out = itertools.cycle(xrange(128))
+
+                yield self.rreset()
+                yield self.wreset()
+                yield self.fifo_state_check(WFull=0, REmpty=1)
+
+                for _ in range(5):
+                    # Fill up to DEPTH-1
+                    do = data_out.next()
+                    for i in range(DEPTH-1):
+                        yield self.fifo_write(data_in.next())
+                        yield self.fifo_state_check(WFull=0, REmpty=None, RData=None)
+                    # Fill up to DEPTH
+                    yield self.fifo_write(data_in.next())
+                    yield self.fifo_state_check(WFull=1, REmpty=None, RData=None)
+                    # Drain down to 1
+                    for i in range(DEPTH-1):
+                        yield self.fifo_read()
+                        yield self.fifo_state_check(WFull=None, REmpty=0, RData=None)
+                    # Drain to 0
+                    yield self.fifo_read()
+                    yield self.fifo_state_check(WFull=0, REmpty=1)
+                    # Write, Read (to move the pointers one step further)
+                    yield self.fifo_write(data_in.next())
+                    for _ in range(3): yield self.rclk.posedge # Waith for clock domain crossing sync and update of empty
+                    yield self.fifo_read()
+                    data_out.next()
+
+                yield self.wclk.posedge
+                yield self.rclk.posedge
+
+                raise StopSimulation
+            return _inst
+
+        getDut = sim.DUTer()
+
+        for s in self.simulators:
+            getDut.selectSimulator(s)
+            for dpt in DEPTH:
+                dut = getDut( fifo_async,
+                              wrst=self.wrst,
+                              rrst=self.rrst,
+                              wclk=self.wclk,
+                              rclk=self.rclk,
+                              wfull=self.wfull,
+                              we=self.we,
+                              wdata=None, # <-- Not connected
+                              rempty=self.rempty,
+                              re=self.re,
+                              rdata=None, # <-- Not connected
+                              depth=dpt,
+                              width=None
+                            )
+                stm = stim(dpt)
+                Simulation(self.wclkgen, self.rclkgen, dut, stm).run()
+                del dut, stm
+
+
+
+if __name__ == '__main__':
+    #import sys;sys.argv = ['', 'Test.testFifo']
+    unittest.main()
