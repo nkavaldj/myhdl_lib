051912de1e468f0db5d1e4ecc3b8c8d60107601f
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2016-02-26 22:31:12 +0100

    Made payload_generator visible through __init__.py

--- myhdl_lib/simulation/__init__.py
+++ myhdl_lib/simulation/__init__.py
@@ -1,7 +1,9 @@
 from _DUTer import DUTer
 from clock import Clock
 from reset import ResetSync
+from payload_generator import payload_generator
 
-__all__ =["DUTer", 
+__all__ =["DUTer",
           "Clock",
-          "ResetSync"]
+          "ResetSync",
+          "payload_generator"]
