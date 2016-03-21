a09fc18a827217448b8c3cf4684d958d29324435
hristo <h.n.nikolov@gmail.com>
2016-03-21 21:49:15 +0100

    Use 'setuptools' to enable installation in develop mode, i.e., install by 'python setup.py develop'. This allows also for an easy uninstall.

--- setup.py
+++ setup.py
@@ -1,6 +1,35 @@
-from distutils.core import setup
+""" setuptools distribution and installation script. """
 
-setup(name='myhdl_lib',
-      version='0.1',
-      packages=['myhdl_lib', 'myhdl_lib.simulation'],
-      )
+from setuptools import setup
+
+
+#def readme():
+#    with open('README.rst') as f:
+#        return f.read()
+
+
+setup( name                 = 'myhdl_lib',
+       version              = '0.1',
+       description          = 'TODO: Library of components based on Python and MyHDL',
+# TODO      long_description     = readme(),
+       classifiers          = [
+           'Development Status   :: 3 - Alpha',
+           'License              :: MIT License',
+           'Programming Language :: Python :: 2.7',
+           'Topic                :: HW design',
+       ],
+       keywords             = 'myhdl pihdf fpga',
+       url                  = 'https://github.com/nkavaldj/myhdl_lib.git',
+       author               = 'Nikolay Kavaldjiev',
+       author_email         = 'nikolay.kavaldjiev@gmail.com',
+       license              = 'MIT',
+       packages             = [
+           'myhdl_lib',
+           'myhdl_lib.simulation'
+       ],
+       install_requires     = [
+           'myhdl'
+       ],
+       include_package_data = True,
+       zip_safe             = False
+    )
