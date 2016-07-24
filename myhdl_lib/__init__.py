9902afd2031e7847373f692821b2135fd0810aa8
Nikolay Kavaldjiev <nikolay.kavaldjiev@gmail.com>
2016-07-24 16:12:13 +0200

    Added modules checksum and bytecount to __init__

--- myhdl_lib/__init__.py
+++ myhdl_lib/__init__.py
@@ -8,6 +8,7 @@ from myhdl_lib.handshake import hs_join, hs_fork, hs_mux, hs_demux, hs_arbmux, h
 from myhdl_lib.arbiter import arbiter, arbiter_priority, arbiter_roundrobin
 from myhdl_lib.pipeline_control import pipeline_control
 from myhdl_lib.utils import assign, byteorder
+from myhdl_lib.stream import bytecount, checksum
 
 __all__ = [
            "rom", "ram_sp_rf", "ram_sp_wf", "ram_sp_ar", "ram_sdp_rf", "ram_sdp_wf", "ram_sdp_ar", "ram_dp_rf", "ram_dp_wf", "ram_dp_ar",
@@ -17,6 +18,7 @@ __all__ = [
            "hs_join", "hs_fork", "hs_mux", "hs_demux", "hs_arbmux", "hs_arbdemux",
            "arbiter", "arbiter_priority", "arbiter_roundrobin",
            "pipeline_control",
-           "assign", "byteorder"
+           "assign", "byteorder",
+           "bytecount", "checksum"
            ]
 
